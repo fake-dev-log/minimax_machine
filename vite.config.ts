2ae4d36aaabbd33807a4207b2a10c17853810f50
fake.dev.log <fake.dev.log@gmail.com>
2025-08-06 01:53:54 +0900

    Update vite confg

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/minmax_machine/',
+  base: '/minimax_machine/',
   plugins: [react(), tailwindcss()],
 })
